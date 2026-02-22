09e623a1f0d6897ff5ee5a39f769347bb74b4b70
Srishruti24 <sri.shruti24@gmail.com>
2026-02-22 18:13:31 +0530

    fix:fix bug in flight update

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -51,7 +51,7 @@ class FlightRepository extends CrudRepository{
     async updateRemainingSeats(flightId,seats,dec=true){
         await db.sequelize.query(addRowLockOnFlights(flightId));//row lock for us if any update we want to do
         const Flight=await flight.findByPk(flightId);
-        if(parseInt(dec)){
+        if(+dec){
         await Flight.decrement('totalSeats',{by:seats});          
         }else{
         await Flight.increment('totalSeats',{by:seats});     
